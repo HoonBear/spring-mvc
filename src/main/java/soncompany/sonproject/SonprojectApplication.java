a8d05b0d431c756b7e4d46c81172a13b733c17d0
손정훈 <sonjeonghun@sonjeonghun-ui-MacBookPro.local>
2022-06-23 02:15:56 +0900

    Feat: hello world

---
+++ src/main/java/soncompany/sonproject/SonprojectApplication.java
@@ -0,0 +1,13 @@
+package soncompany.sonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soncompany/sonproject/SonprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package soncompany.sonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
